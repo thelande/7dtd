25dbe8461ac681b82bd0d255a84877f16aa82b78
Tom Helander <thomas.helander@gmail.com>
2024-08-24 09:59:21 -0700

    Fix variable usage in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -eu
 
 PATH=$PATH:/usr/games
 DO_VALIDATE="${DO_VALIDATE:-0}"
+SKIP_UPDATE="${SKIP_UPDATE:-0}"
 
 mkdir -p "$INSTALL_DIR"
 chown -R 7days:7days "$INSTALL_DIR"
@@ -13,10 +14,10 @@ echo "Setting timezone to $TIMEZONE"
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 echo "$TIMEZONE" > /etc/timezone
 
-if [[ -n "$SKIP_UPDATE" && $SKIP_UPDATE -ne 1 ]]; then
+if [[ $SKIP_UPDATE -eq 0 ]]; then
     echo "Installing/updating 7 Days to Die Dedicated Server"
     STEAMCMD_OPTS=(+force_install_dir "$INSTALL_DIR" +login anonymous +app_update $APP_ID)
-    [[ -n "$DO_VALIDATE" && $DO_VALIDATE -eq 1 ]] && STEAMCMD_OPTS+=(validate)
+    [[ $DO_VALIDATE -eq 1 ]] && STEAMCMD_OPTS+=(validate)
     STEAMCMD_OPTS+=(+quit)
 
     chown -R 7days:7days /home/7days
